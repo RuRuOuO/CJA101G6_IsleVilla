37da06386c88a5210e526a64ccb7a8482fbeee01
Blaire <judy581107@gmail.com>
2025-06-10 10:56:07 +0800

    專案結構+pom.xml 更新

--- src/main/java/com/islevilla/product/controller/ProductController.java
+++ src/main/java/com/islevilla/product/controller/ProductController.java
@@ -1,26 +1,24 @@
 package com.islevilla.product.controller;
 
-import com.islevilla.product.model.Product;
 import com.islevilla.product.model.ProductService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-@RestController
-@RequestMapping("/products")
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+@Controller
+@RequestMapping("/product")
 public class ProductController {
 
-    @Autowired
-    private ProductService productService;  // 注入服務層
-
-    @PostMapping
-    public String addProduct(@RequestBody Product product) {
-        int productId = productService.addProduct(product);  // 呼叫服務層的 addProduct 方法
-        if (productId > 0) {
-            return "Product added successfully with ID: " + productId;
-        } else {
-            return "Error occurred while adding product.";
-        }
+    private final ProductService productService;
+    public ProductController(ProductService productService) {
+        this.productService = productService;
     }
+
+    @GetMapping("/list")
+    public String listProducts(Model model) {
+        model.addAttribute("products", productService.getAllProducts());
+        return "product_index";    // 對應 src/main/resources/templates/product_index.html
+    }
+
+
 }
