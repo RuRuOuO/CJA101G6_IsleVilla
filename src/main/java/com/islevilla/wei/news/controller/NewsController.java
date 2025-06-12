3b0f9556afc937d8f90642d94595bacf3fabfad4
Vail Chen <vivi88605@gmail.com>
2025-06-13 00:47:05 +0800

    backup

--- src/main/java/com/islevilla/wei/news/controller/NewsController.java
+++ src/main/java/com/islevilla/wei/news/controller/NewsController.java
@@ -1,16 +1,24 @@
 package com.islevilla.wei.news.controller;
 
+import com.islevilla.wei.news.model.NewsService;
+import com.islevilla.wei.news.model.NewsVO;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
 
 @Controller
 public class NewsController {
 
-    @GetMapping("/news")
-    public String newsList() {
-        // 添加需要的數據
-        // model.addAttribute("newsList", newsService.getAllNews());
+    @Autowired
+    private NewsService newsService;
 
+    @GetMapping("/news")
+    public String newsList(Model model) {
+        List<NewsVO> newsList = newsService.getAll();
+        model.addAttribute("newsList", newsList);
         return "front-end/news/newsList";
     }
 }
