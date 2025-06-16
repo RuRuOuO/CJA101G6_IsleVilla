79c4ad974e5bc947d9dddd4d9377ae537fd17923
Vail Chen <vivi88605@gmail.com>
2025-06-16 21:52:32 +0800

    修正首頁路由

--- src/main/java/com/islevilla/wei/homepage/HomePageController.java
+++ src/main/java/com/islevilla/wei/homepage/HomePageController.java
@@ -12,13 +12,18 @@ import java.util.List;
 @Controller
 public class HomePageController {
 
-        @Autowired
-        private NewsService newsService;
+    @Autowired
+    private NewsService newsService;
 
-        @GetMapping("/")
-        public String index(Model model){
-            List<News> latest3News = newsService.getLatestThreeNews();
-            model.addAttribute("latestNews", latest3News); // ✅ 有加這行才不會報錯
-            return "front-end/index";
-        }
+    @GetMapping("/")
+    public String index(Model model) {
+        List<News> latest3News = newsService.getLatestThreeNews();
+        model.addAttribute("latestNews", latest3News); // ✅ 有加這行才不會報錯
+        return "front-end/index";
+    }
+
+    @GetMapping("/member")
+    public String member() {
+        return "front-end/member/memberInfo";
+    }
 }
