3d0513094d3c50c5282452921b0c8387f084fab1
sandy chan <sandychan19980106@gmail.com>
2025-06-17 12:43:19 +0800

    Update some files.

--- src/main/java/com/islevilla/chen/roomType/model/RoomTypeRepository.java
+++ src/main/java/com/islevilla/chen/roomType/model/RoomTypeRepository.java
@@ -2,9 +2,12 @@ package com.islevilla.chen.roomType.model;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 @Repository
+@Transactional
 public interface RoomTypeRepository extends JpaRepository<RoomTypeVO, Integer>{
+
 
 }
 
--- src/main/java/com/islevilla/chen/roomTypeAvailability/model/RoomTypeAvailabilityRepository.java
+++ src/main/java/com/islevilla/chen/roomTypeAvailability/model/RoomTypeAvailabilityRepository.java
@@ -2,8 +2,10 @@ package com.islevilla.chen.roomTypeAvailability.model;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 @Repository
+@Transactional
 public interface RoomTypeAvailabilityRepository extends JpaRepository<RoomTypeAvailabilityVO, Integer>{
 
 }
