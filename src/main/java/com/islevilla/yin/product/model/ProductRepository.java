f0266cf097484616d73d7ba15771721c9d60bbf7
Blaire <judy581107@gmail.com>
2025-07-07 14:50:50 +0800

    商品分頁設定

--- src/main/java/com/islevilla/yin/product/model/ProductRepository.java
+++ src/main/java/com/islevilla/yin/product/model/ProductRepository.java
@@ -1,5 +1,7 @@
 package com.islevilla.yin.product.model;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
@@ -10,4 +12,10 @@ public interface ProductRepository extends JpaRepository<Product, Integer> {
     List<Product> findByProductStatus(Byte status);
     
     List<Product> findByProductCategoryProductCategoryIdAndProductStatus(Integer productCategoryId, Byte status);
+
+    Page<Product> findByProductStatus(Byte status, Pageable pageable);
+
+    Page<Product> findByProductCategoryProductCategoryIdAndProductStatus(Integer productCategoryId, Byte status, Pageable pageable);
+
+    Page<Product> findByProductCategoryProductCategoryId(Integer productCategoryId, Pageable pageable);
 }
